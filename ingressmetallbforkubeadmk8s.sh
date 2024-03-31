8acc9cd50fe63db6e7bcbc2331b1b3521ade9d27
Andy Wang <yagosys@gmail.com>
2024-03-31 09:42:56 +0800

    first

---
+++ ingressmetallbforkubeadmk8s.sh
@@ -0,0 +1,21 @@
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.14.3/config/manifests/metallb-native.yaml
+kubectl rollout status deployment controller -n metallb-system
+
+local_ip=$(kubectl get node -o wide | grep 'control-plane' | awk '{print $6}')
+cat <<EOF | tee $HOME/metallbippool.yaml
+apiVersion: metallb.io/v1beta1
+kind: IPAddressPool
+metadata:
+  name: first-pool
+  namespace: metallb-system
+spec:
+  addresses:
+  - $local_ip/32
+---
+apiVersion: metallb.io/v1beta1
+kind: L2Advertisement
+metadata:
+  name: example
+  namespace: metallb-system
+EOF
+kubectl apply -f $HOME/metallbippool.yaml
